63ce08370f6671b7fde89721b0c45f961f9ac1c0
sainigma <sainigma@gmail.com>
2022-02-08 18:03:33 +0200

    added methods for sending player mockup data

--- main.py
+++ main.py
@@ -1,17 +1,12 @@
 import socket
 import json
-
-def send_data(uri : str, port : int, data : dict):
-  data['data'] = json.dumps(data['data'])
-  payload = bytes(json.dumps(data), 'utf-8')
-  connection = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-  connection.sendto(payload, (uri, port))
+import time
 
 transform_data = {
   "id":"player id_here",
   "data": {
     "pos": (1.23456, 1.23456, 1.23456),
-    "dir": (1.23456, 1.23456, 1.23456)
+    "rot": (1.23456, 1.23456, 1.23456)
   }
 }
 
@@ -31,7 +26,36 @@ sensor_data_2 = {
   }
 }
 
-packets = [transform_data, sensor_data_1, sensor_data_2]
+def send_data(uri : str, port : int, data : dict):
+  payload = {
+    'id': data['id'],
+    'data': json.dumps(data['data'])
+  }
+  payload = bytes(json.dumps(payload), 'utf-8')
+  connection = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+  connection.sendto(payload, (uri, port))
 
-for packet in packets:
-  send_data("localhost", 8000, packet)
+def send_all(uri : str, port : int):
+  packets = [transform_data, sensor_data_1, sensor_data_2]
+
+  for packet in packets:
+    send_data(uri, port, packet)
+
+def move_player(uri : str, port : int):
+  x = 0
+  y = 0
+  z = 0
+  while True:
+    data = transform_data
+    data['data']['pos'] = {'x':x, 'y':y, 'z':z}
+    data['data']['rot'] = {'x':0, 'y':0, 'z':0}
+    print(data)
+    send_data(uri, port, data)
+
+    x += 0.1
+    z += 0.1
+
+    time.sleep(1)
+
+#send_all("localhost", 9000)
+move_player("localhost", 9000)

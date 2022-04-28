b3f2c1dea322b41aea7236489572750663134cac
sainigma <sainigma@gmail.com>
2022-04-28 15:16:26 +0300

    updated methods for current protocol

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ def random_room_status():
 
 def get_stock_messages():
   messages = []
-  with open('./sample_data.log', 'r') as f:
+  with open('./packets/sample_data.log', 'r') as f:
     messages = f.readlines()
   return messages
 
@@ -61,8 +61,19 @@ def send_string(data):
   result = result.string(data).get()
   send_bytes('localhost', 9000, result)
 
-messages = get_stock_messages()
-send_string(messages[1])
+def send_motion(roomName):
+  data = r'''{"topic":"rooms/''' + roomName + r'''/motion/d1f61303-e2b2-40c2-a203-b752926b93fc","data":{"payload":{"type":"motionSensor","roomName":"haskell","roomId":"04fba3a7-a0c4-4058-8b76-282c2d9f2da3","timestamp":1650969558487},"signatures":[{"protected":{"timestamp":1650969558487,"messageid":"mQPLV7D4XfAr","alg":"ES512","kid":"2rCGPtxCjl3uTns6U6mMtjwAD3vjVlL1OcfgXP_Llk8"},"signature":"APmFqjpdBLUPv4H4Oq_RvmSZX4oM320kgeqCTEL3q_g9mp2GoGLlDvpDvfw28RslNIjN8szb7cEERbkbg_Z89ZfIAeTxn2x68EWb_JcxmTXLvkXumdWCDwj1MeOTVK5yhtGgzeRaqpz6X3kjJOweqGKcMR0c2DoOtrvFnbAS-fcWnERe"}]}}'''
+  send_string(data)
+
+def send_reservation(roomName, reserved = True):
+  reservation = "VARATTU" if reserved else "VAPAA"
+  data = r'''{"topic":"rooms/''' + roomName + r'''/reservation/d1f61303-e2b2-40c2-a203-b752926b93fc","data":{"payload":{"currentEndTime":1645704000000,"currentStartTime":1645696800000,"currentId":"495-1645696800000","currentTopic":"''' + reservation + r'''\\u2022RESERVERAT\\u2022RESERVED"},"signatures":[{"protected":{"timestamp":1650969558487,"messageid":"mQPLV7D4XfAr","alg":"ES512","kid":"2rCGPtxCjl3uTns6U6mMtjwAD3vjVlL1OcfgXP_Llk8"},"signature":"APmFqjpdBLUPv4H4Oq_RvmSZX4oM320kgeqCTEL3q_g9mp2GoGLlDvpDvfw28RslNIjN8szb7cEERbkbg_Z89ZfIAeTxn2x68EWb_JcxmTXLvkXumdWCDwj1MeOTVK5yhtGgzeRaqpz6X3kjJOweqGKcMR0c2DoOtrvFnbAS-fcWnERe"}]}}'''
+  send_string(data)
+
+#send_motion('java')
+send_reservation('java')
+#messages = get_stock_messages()
+#send_string(messages[0])
 
 exit()
 
